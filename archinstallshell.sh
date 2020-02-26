1e68aaf8fd6be925009efc60e2f4b5de81c0b36f
ThorstenHeck <simonparmesan@outlook.de>
2020-02-26 02:32:13 +0100

    Update archinstallshell.sh

--- archinstallshell.sh
+++ archinstallshell.sh
@@ -79,7 +79,8 @@ arch-chroot /mnt/ grub-mkconfig -o /boot/grub/grub.cfg
 umount_partitions(){
   mounted_partitions=(`lsblk | grep '/mnt' | awk '{print $7}' | sort -r`)
   swapoff -a
-  for i in ${mounted_partitions[@]}; do
+  for i in "${mounted_partitions[@]}"; do
     umount $i
   done
 }
+umount_partitions
