9c9bb2ce96539e371aa81c78697a8a0cd8604fa8
ThorstenHeck <simonparmesan@outlook.de>
2023-06-13 15:47:02 +0200

    Update archinstallshell.sh
    Made new for X1 Carbon deployment

--- archinstallshell.sh
+++ archinstallshell.sh
@@ -1,59 +1,53 @@
 #!/bin/bash
-EFI_MOUNTPOINT="/boot"
-ROOT_MOUNTPOINT="/dev/sda1"
-BOOT_MOUNTPOINT="/dev/sda"
 MOUNTPOINT="/mnt"
-EDITOR="nano"
-device=`lsblk -do name,tran | grep 'sd\|hd\|vd\|nvme\|mmcblk' | grep -v 'usb' | awk '{print "/dev/" $1}'`
-BOOT_MOUNTPOINT=$device
-ram=`free -t -m | grep 'Mem:' | awk '{print $2}'`
-swap=$((ram * 2))
-swapsect=$((swap * 2048))
-sgdisk -og $device
-sgdisk -n 1:2048:1050623 -c 1:"EFI System Partition" -t 1:ef00 $device
-sectorstart=`sgdisk -F $device
-sgdisk -n 2:$sectorstart:$swapsect -c 2:"Swap Partition" -t 2:8200 $device
-sectorstart=`sgdisk -F $device`
-sectorend=`sgdisk -E $device`
-sgdisk -n 3:$sectorstart:$sectorend -c 3:"Root" -t 3:8300 $device
+DEVICE=$(lsblk -n -do name,tran | grep -v 'loop' | grep -v 'usb' | awk '{print "/dev/" $1}'`
+RAM=$(free -t -m | grep 'Mem:' | awk '{print $2}')
+SWAP=$((RAM * 2))
+SWAPSECT=$((swap * 2048))
+sgdisk -og $DEVICE
+sgdisk -n 1:2048:1050623 -c 1:"EFI System Partition" -t 1:ef00 $DEVICE
+SECTORSTART=$(sgdisk -F $DEVICE)
+sgdisk -n 2:$SECTORSTART:$SWAPSECT -c 2:"Swap Partition" -t 2:8200 $DEVICE
+SECTORSTART=$(sgdisk -F $DEVICE)
+SECTOREND=$(sgdisk -E $DEVICE)
+sgdisk -n 3:$SECTORSTART:$SECTOREND -c 3:"Root" -t 3:8300 $DEVICE
+
+PARTUEFI=$(lsblk -no name,partlabel | grep 'EFI System Partition' | sed 's/^..//' | awk '{print "/dev/" $1}')
+PARTROOT=$(lsblk -no name,partlabel | grep 'Root' | sed 's/^..//' | awk '{print "/dev/" $1}')
+PARTSWAP=$(lsblk -no name,partlabel | grep 'Swap Partition' | sed 's/^..//' | awk '{print "/dev/" $1}')
+mkfs.fat -F 32 -n EFIBOOT $PARTUEFI
+yes | mkfs.ext4 -L p_arch $PARTROOT
+mkswap -L p_swap $PARTSWAP
+mount -L p_arch $MOUNTPOINT  
+mkdir -p ${MOUNTPOINT}/boot  
+mount -L EFIBOOT ${MOUNTPOINT}/boot
+swapon -L p_swap 
+
 pacman -Sy --noconfirm
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 pacman -S reflector --noconfirm
 reflector --verbose --country 'Germany' --latest 10 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-partuefi=`lsblk -no name,partlabel | grep 'EFI System Partition' | awk '{print $1}' | sed 's/^..//' | awk '{print "/dev/" $1}'`
-partroot=`lsblk -no name,partlabel | grep 'Root' | awk '{print $1}' | sed 's/^..//' | awk '{print "/dev/" $1}'`
-partswap=`lsblk -no name,partlabel | grep 'Swap Partition' | awk '{print $1}' | sed 's/^..//' | awk '{print "/dev/" $1}'`
-mkfs.fat -F 32 -n EFIBOOT $partuefi
-y | mkfs.ext4 -L p_arch $partroot
-mkswap -L p_swap $partswap
-mount -L p_arch /mnt  
-mkdir -p /mnt/boot  
-mount -L EFIBOOT /mnt/boot  
-swapon -L p_swap 
-pacstrap $MOUNTPOINT base base-devel linux linux-firmware nano
-genfstab -Up $MOUNTPOINT > /mnt/etc/fstab
-echo "KEYMAP=$KEYMAP" > ${MOUNTPOINT}/etc/vconsole.conf
-host_name='archlinux'
-echo $host_name > /etc/hostname
+
+pacstrap -K $MOUNTPOINT base base-devel linux linux-firmware nano
+genfstab -U $MOUNTPOINT > ${MOUNTPOINT}/etc/fstab
+
+HOSTNAME='archlinux'
 ZONE='Europe'
 SUBZONE='Berlin'
-arch-chroot /mnt/ ln -sf /usr/share/zoneinfo/${ZONE}/${SUBZONE} /etc/localtime
-echo LANG=de_DE.UTF-8 > /etc/locale.conf
-arch-chroot /mnt/ sed -i 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
-arch-chroot /mnt/ sed -i 's/#de_DE ISO-8859-1/de_DE ISO-8859-1/' /etc/locale.gen
-arch-chroot /mnt/ sed -i 's/#de_DE@euro ISO-8859-15/de_DE@euro ISO-8859-15/' /etc/locale.gen
-arch-chroot /mnt/ locale-gen
-pacman -Sy
-arch-chroot /mnt/ mkinitcpio -p linux
-pacman --root $MOUNTPOINT -S efibootmgr dosfstools gptfdisk grub --noconfirm
-arch-chroot /mnt/ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck --debug
-arch-chroot /mnt/ grub-mkconfig -o /boot/grub/grub.cfg
+KEYMAP='de-latin1'
+LANG='de_DE.UTF-8'
 
-umount_partitions(){
-  mounted_partitions=(`lsblk | grep '/mnt' | awk '{print $7}' | sort -r`)
-  swapoff -a
-  for i in "${mounted_partitions[@]}"; do
-    umount $i
-  done
-}
-umount_partitions
+arch-chroot $MOUNTPOINT ln -sf /usr/share/zoneinfo/${ZONE}/${SUBZONE} /etc/localtime
+arch-chroot $MOUNTPOINT echo $HOSTNAME > /etc/hostname
+arch-chroot $MOUNTPOINT echo LANG=$LANG > /etc/locale.conf
+arch-chroot $MOUNTPOINT echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+
+arch-chroot $MOUNTPOINT sed -i 's/#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen
+arch-chroot $MOUNTPOINT sed -i 's/#de_DE ISO-8859-1/de_DE ISO-8859-1/' /etc/locale.gen
+arch-chroot $MOUNTPOINT sed -i 's/#de_DE@euro ISO-8859-15/de_DE@euro ISO-8859-15/' /etc/locale.gen
+arch-chroot $MOUNTPOINT locale-gen
+arch-chroot $MOUNTPOINT mkinitcpio -p linux
+pacman --root $MOUNTPOINT -S efibootmgr dosfstools gptfdisk grub --noconfirm
+arch-chroot $MOUNTPOINT grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck --debug
+arch-chroot $MOUNTPOINT grub-mkconfig -o /boot/grub/grub.cfg
+
+umount -r $MOUNTPOINT
