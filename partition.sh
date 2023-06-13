1796f467ce683f1190779d9a04d851d0516bf939
ThorstenHeck <simonparmesan@outlook.de>
2023-06-13 17:51:05 +0200

    debug

--- partition.sh
+++ partition.sh
@@ -8,9 +8,12 @@ sgdisk -og $DEVICE
 sgdisk -n 1:2048:1050623 -c 1:"EFI System Partition" -t 1:ef00 $DEVICE
 SECTORSTART=$(sgdisk -F $DEVICE)
 sgdisk -n 2:$SECTORSTART:$SWAPSECT -c 2:"Swap Partition" -t 2:8200 $DEVICE
+echo $SECTORSTART
 SECTORSTART=$(sgdisk -F $DEVICE)
 SECTOREND=$(sgdisk -E $DEVICE)
 sgdisk -n 3:$SECTORSTART:$SECTOREND -c 3:"Root" -t 3:8300 $DEVICE
+echo $SECTORSTART
+echo $SECTOREND
 sleep 2
 PARTUEFI=$(lsblk -no name,partlabel | grep 'EFI System Partition' | sed 's/^..//' | awk '{print "/dev/" $1}')
 PARTROOT=$(lsblk -no name,partlabel | grep 'Root' | sed 's/^..//' | awk '{print "/dev/" $1}')
